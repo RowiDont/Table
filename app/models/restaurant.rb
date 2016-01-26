5eaa05c370f2e0aabe94d5b486fa3c6d613ec3ba
App Academy Student <appacademy@Apps-Mac-mini-78.local>
2016-01-26 14:15:07 -0500

    Create restaurant and city routes.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ActiveRecord::Base
   has_one :city, through: :postalcode
 
   belongs_to :postalcode, foreign_key: "postal_code", primary_key: "code"
+  
+  # TODO: Add validations for when admins create restaurants
 end
